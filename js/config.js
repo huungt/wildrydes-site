3ddc14ccae0f7c015bbe515cc92c9747c02892aa
Huy Hung Tran <huy.hung.tran@stud.uni-hannover.de>
2025-04-16 01:45:12 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_fumkow9d5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6q0eplceg8f6qdn0gorf6mdu0j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '42pc685qke3n8olk25823vhcqr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
